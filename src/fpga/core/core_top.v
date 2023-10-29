838d55bb521dbfc3866507fa4d30f1c7ed69ff51
Alexander Bragdon <alex@alexbragdon.com>
2023-10-29 19:56:52 -0400

    Revert video change

--- src/fpga/core/core_top.v
+++ src/fpga/core/core_top.v
@@ -506,55 +506,158 @@ always @(posedge clk_core_67108864) begin
 	ce_cpu   <= !div[3:0];
 end
 
+// Gameboy
 gb gb (
     .reset(~pll_core_locked),
 
-    .clk_sys    (clk_core_67108864),
-	 .ce         (clk4             ),
-	 .ce_2x      (clk8             ),
+    .clk_sys    ( clk_core_67108864 ),
+	 .ce         ( clk4              ),
+	 .ce_2x      ( clk8              ),
 
     // Audio
-    .audio_l(audio_l),
-    .audio_r(audio_r)
+    .audio_l     ( audio_l          ),
+    .audio_r     ( audio_r          ),
+	 
+	 // interface to the lcd
+	.lcd_clkena   ( lcd_clkena       ),
+	.lcd_data     ( lcd_data         ),
+	.lcd_mode     ( lcd_mode         ),
+	.lcd_on       ( lcd_on           )
 );
 
 // video generation
-wire h_blank;
-wire v_blank;
-wire video_hs_snes;
-wire video_vs_snes;
-wire [23:0] video_rgb_snes;
+wire lcd_clkena;
+wire [14:0] lcd_data;
+wire [1:0] lcd_mode;
+wire lcd_on;
 
-reg video_de_reg;
-reg video_hs_reg;
-reg video_vs_reg;
-reg [23:0] video_rgb_reg;
+// the lcd to vga converter
+wire [5:0] video_r, video_g, video_b;
+wire video_hs, video_vs;
 
-assign video_rgb_clock = clk_74a;
-assign video_rgb_clock_90 = clk_74a;
-assign video_de = video_de_reg;
-assign video_hs = video_hs_reg;
-assign video_vs = video_vs_reg;
-assign video_rgb = video_rgb_reg;
+lcd lcd (
+	 .clk    ( clk64      ),
+	 .pclk_en( ce_pix     ),
+	 .clk4_en( clk4       ),
 
-reg hs_prev;
-reg [2:0] hs_delay;
-reg vs_prev;
+	 .tint   ( 0          ),
+	 .isGBC  ( 0          ),
 
-always @(posedge clk_74a) begin
-	video_hs_reg  <= 0;
-	video_de_reg  <= 0;
-	video_rgb_reg <= 24'h0;
+	 // serial interface
+	 .clkena ( lcd_clkena ),
+	 .data   ( lcd_data   ),
+	 .mode   ( lcd_mode   ),  // used to detect begin of new lines and frames
+	 .on     ( lcd_on     ),
+	 
+	 .hs    ( video_hs    ),
+	 .vs    ( video_vs    ),
+	 .r     ( video_r     ),
+	 .g     ( video_g     ),
+	 .b     ( video_b     )
+);
 
-	if (~(h_blank || v_blank)) begin
-		video_de_reg  <= 1;
-		video_rgb_reg <= video_rgb_snes;
-   end
+// video generation
+// ~12,288,000 hz pixel clock
+//
+// we want our video mode of 320x240 @ 60hz, this results in 204800 clocks per frame
+// we need to add hblank and vblank times to this, so there will be a nondisplay area. 
+// it can be thought of as a border around the visible area.
+// to make numbers simple, we can have 400 total clocks per line, and 320 visible.
+// dividing 204800 by 400 results in 512 total lines per frame, and 240 visible.
+// this pixel clock is fairly high for the relatively low resolution, but that's fine.
+// PLL output has a minimum output frequency anyway.
 
-	video_hs_reg <= ~hs_prev && video_hs_snes;
-	video_vs_reg <= ~vs_prev && video_vs_snes;
-	hs_prev <= video_hs_snes;
-	vs_prev <= video_vs_snes;
+
+assign video_rgb_clock = clk_core_12288;
+assign video_rgb_clock_90 = clk_core_12288_90deg;
+assign video_rgb = vidout_rgb;
+assign video_de = vidout_de;
+assign video_skip = vidout_skip;
+assign video_vs = vidout_vs;
+assign video_hs = vidout_hs;
+
+    localparam  VID_V_BPORCH = 'd10;
+    localparam  VID_V_ACTIVE = 'd240;
+    localparam  VID_V_TOTAL = 'd512;
+    localparam  VID_H_BPORCH = 'd10;
+    localparam  VID_H_ACTIVE = 'd320;
+    localparam  VID_H_TOTAL = 'd400;
+
+    reg [15:0]  frame_count;
+
+    reg [9:0]   x_count;
+    reg [9:0]   y_count;
+
+    wire [9:0]  visible_x = x_count - VID_H_BPORCH;
+    wire [9:0]  visible_y = y_count - VID_V_BPORCH;
+
+    reg [23:0]  vidout_rgb;
+    reg         vidout_de, vidout_de_1;
+    reg         vidout_skip;
+    reg         vidout_vs;
+    reg         vidout_hs, vidout_hs_1;
+
+    reg [9:0]   square_x = 'd135;
+    reg [9:0]   square_y = 'd95;
+
+always @(posedge clk_core_12288 or negedge reset_n) begin
+
+    if(~reset_n) begin
+
+        x_count <= 0;
+        y_count <= 0;
+
+    end else begin
+        vidout_de <= 0;
+        vidout_skip <= 0;
+        vidout_vs <= 0;
+        vidout_hs <= 0;
+
+        vidout_hs_1 <= vidout_hs;
+        vidout_de_1 <= vidout_de;
+
+        // x and y counters
+        x_count <= x_count + 1'b1;
+        if(x_count == VID_H_TOTAL-1) begin
+            x_count <= 0;
+
+            y_count <= y_count + 1'b1;
+            if(y_count == VID_V_TOTAL-1) begin
+                y_count <= 0;
+            end
+        end
+
+        // generate sync 
+        if(x_count == 0 && y_count == 0) begin
+            // sync signal in back porch
+            // new frame
+            vidout_vs <= 1;
+            frame_count <= frame_count + 1'b1;
+        end
+
+        // we want HS to occur a bit after VS, not on the same cycle
+        if(x_count == 3) begin
+            // sync signal in back porch
+            // new line
+            vidout_hs <= 1;
+        end
+
+        // inactive screen areas are black
+        vidout_rgb <= 24'h0;
+        // generate active video
+        if(x_count >= VID_H_BPORCH && x_count < VID_H_ACTIVE+VID_H_BPORCH) begin
+
+            if(y_count >= VID_V_BPORCH && y_count < VID_V_ACTIVE+VID_V_BPORCH) begin
+                // data enable. this is the active region of the line
+                vidout_de <= 1;
+
+                vidout_rgb[23:16] <= 8'd60;
+                vidout_rgb[15:8]  <= 8'd60;
+                vidout_rgb[7:0]   <= 8'd60;
+
+            end 
+        end
+    end
 end
 
 //
